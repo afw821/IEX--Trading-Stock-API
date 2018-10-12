29c10cd328a3016b47fc598b225b00649f6df502
afw821 <afw821@gmail.com>
2018-10-12 09:57:45 -0400

    I added a function to add favorite stocks to a list

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const displayStockInfo = function () {
   $.ajax({
     url: queryURL,
     method: 'GET'
-  }).then(function(response) {
+  }).then(function (response) {
     //console.log response to test
-  console.log(response);
+    console.log(response);
     //Use response to create const for items in the object
     const companyName = response.quote.companyName;
     const stockSymbol = response.quote.symbol;
@@ -32,13 +32,13 @@ const displayStockInfo = function () {
         <td>${stockPrice}</td>
         <td>${companyNews}</td>
       </tr>)`);
-   
+
     $('.stocks').append(nameHolder);
   });
 
 }
 //This listener allows stock info to be appended when stock btn is pushed
-$('#buttons-view').on('click', '.stock-btn', displayStockInfo); 
+$('#buttons-view').on('click', '.stock-btn', displayStockInfo);
 
 // Function for displaying stock buttons initally when page loads
 const render = function () {
@@ -48,13 +48,14 @@ const render = function () {
   // Looping through the array of stocks to render initial buttons
   for (let i = 0; i < stocks.length; i++) {
     // Then dynamicaly generating buttons for each stock in the array
-    const newButton = $('<button>'); 
+    const newButton = $('<button>');
     // Adding a class of stock-btn to our button
-    newButton.addClass('stock-btn');  
+    newButton.addClass('stock-btn');
+    newButton.addClass('btn btn-primary');
     // Adding a data-attribute
-    newButton.attr('data-name', stocks[i]);  
+    newButton.attr('data-name', stocks[i]);
     // Providing the initial button text
-    newButton.text(stocks[i]); 
+    newButton.text(stocks[i]);
     // Adding the button to the buttons-view div
     $('#buttons-view').append(newButton);
   }
@@ -62,58 +63,71 @@ const render = function () {
 // Calling the renderButtons function to display the intial buttons
 render();
 
-// This function handles events where one button is clicked
-// const addButton = function(event) {
-
-//   // event.preventDefault() prevents the form from trying to submit itself.
-//   // We're using a form so that the user can hit enter instead of clicking the button if they want
-//   event.preventDefault();
-
-//   // This line will grab the text from the input box
-//   const stock = $('#stock-input').val().trim();
-  
-//   // The stock from the textbox is then added to our array
-//   stocks.push(stock);
-
-//   // Deletes the contents of the input
-//   $('#stock-input').val('');
-
-//   // calling render which handles the processing of our stock array
-//   render();
-// }
-
-// // Even listener for #add-stock button
-// $('#add-stock').on('click', addButton);
 
 
-
-
-
-const validateSymbol = function(event){
+//This function appends new stock buttons when you enter them
+const validateSymbol = function (event) {
+  //preventDefault prevents
   event.preventDefault();
+  //Variable stock assigned stock input id
   const stock = $('#stock-input').val().trim();
+  //stock input will be uppercase
   const stockUC = stock.toUpperCase();
+  //URL for all data symbols on IEX trading
   const apiSymbols = 'https://api.iextrading.com/1.0/ref-data/symbols';
+  //AJAX Call
   $.ajax({
     url: apiSymbols,
     method: 'GET'
   }).then(function (responseTwo) {
-      const validationList = responseTwo;
-   for(let i = 0; i < validationList.length; i++){
-     if(stockUC === responseTwo[i].symbol){
-       console.log(stock);
-       stocks.push(stockUC);
-       let newButtonHTML = $(`<button class="new-stock-btn" val="${stockUC}">${stockUC}
-       </button>&nbsp;`); 
-       newButtonHTML.on('click', displayStockInfo);
-       $('#buttons-view').append(newButtonHTML);
-       render();
-       return;
-     }else{
-       $('#stock-input').val('');
-     }
-   }
+    const validationList = responseTwo;
+    //Loop through array of objects returned by AJAX
+    for (let i = 0; i < validationList.length; i++) {
+      //IF USER input uppercase strictly equal to stock symbol in AJAX return
+      if (stockUC === responseTwo[i].symbol) {
+        console.log(stock);
+        //Push item in array
+        stocks.push(stockUC);
+        //Dynamically generated stock buttons
+        let newButtonHTML = $(`<button class="new-stock-btn btn btn-primary" val="${stockUC}">${stockUC}
+       </button>&nbsp;`);
+        //Event Listener for newly generated buttons
+        newButtonHTML.on('click', displayStockInfo);
+        //Append newly generated buttons to buttons-view div
+        $('#buttons-view').append(newButtonHTML);
+        render();
+        return;
+      } else {
+        $('#stock-input').val('');
+
+      }
+    }
   });
 };
 $('#add-stock').on('click', validateSymbol);
+
+
+//Add to favorite function
+const addFavorite = function () {
+  const apiSymbols = 'https://api.iextrading.com/1.0/ref-data/symbols';
+  const stock = $('#stock-input').val();
+  const stockUC = stock.toUpperCase();
+  const stockUCButton = $(`<button class="new-stock-btn btn btn-primary" val="${stockUC}">${stockUC}
+</button>&nbsp;`);
+  $.ajax({
+    url: apiSymbols,
+    method: 'GET'
+  }).then(function (responseThree) {
+    const validationListTwo = responseThree;
+    //Loop through array of objects returned by AJAX
+    for (let i = 0; i < validationListTwo.length; i++) {
+      if (stock.length >= 1 && stockUC === responseThree[i].symbol) {
+        $('.favorite-stocks').append(stockUCButton);
+      } else {
+        $('#stock-input').val('');
+      }
+    }
+  });
+}
+$('.favorite').on('click', addFavorite);
 
